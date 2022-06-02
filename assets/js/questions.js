d7b500d2437ff431604461dfb1ebcf4543ab7be7
史开乐 <csurfssecure@gmail.com>
2022-06-02 13:06:15 -0400

    questions

---
+++ assets/js/questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "If BEAT is coded as ‘EHDW’, how will you code ROAD ?",
+    answer: "URDG",
+    options: [
+      "SQCF",
+      "URDG",
+      "TQCF",
+      "UREG"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "_______ is the process of finding errors and fixing them within a program.",
+    answer: "Debugging",
+    options: [
+      "Compiling",
+      "Executing",
+      "Debugging",
+      "Scanning"
+    ]
+  },
+
+    {
+    numb: 8,
+    question: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+    answer: "Loop",
+    options: [
+      "Repeater",
+      "Clone",
+      "Debugger",
+      "Loop"
+    ]
+  },
+
+    {
+    numb: 9,
+    question: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+    answer: "Arrays",
+    options: [
+      "Variables",
+      "Recorders",
+      "Arrays",
+      "Strings"
+    ]
+  },
+
+    {
+    numb: 10,
+    question: "Where is the JavaScript placed inside an HTML document or page?",
+    answer: "In the <body> and <head> sections.",
+    options: [
+      "In the <meta> section.",
+      "In the <footer> section.",
+      "In the <body> and <head> sections.",
+      "In the <title> section."
+    ]
+  },
+];
